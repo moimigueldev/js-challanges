85b8b50b3006d3d6af132aa98fe79684179b5237
Moises Miguel <moimigueldev@gmail.com>
2019-11-18 15:15:58 -0600

    fibonacci

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ const port = 3000;
 
 //modules
 const firstReverse = require('./modules/first-reverse');
-const change = require('./modules/matching-subs');
+const fib = require('./modules/fib');
 
-const s1 = 'Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-0091\nDate: Tues April 9, 2005\nVersion: 6.7\nLevel: Alpha'
-const s11 = 'Program title: Hammer\nAuthor: Tolkien\nCorporation: IB\nPhone: +1-503-555-0090\nDate: Tues March 29, 2017\nVersion: 2.0\nLevel: Release'
-const s12 = 'Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-009\nDate: Tues April 9, 2005\nVersion: 6.7\nLevel: Alpha'
+console.log(fib(20))
 
 
-console.log(change(s1, 'Ladder', '1.1'))
-console.log(change(s11, 'Balance', '1.5.6'))
-// console.log(change(s12, 'Ladder', '1.1'))
+
+
 
 
 app.get('/', async (req, res) => {
