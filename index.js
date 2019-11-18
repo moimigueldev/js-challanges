d653e4bdf6824f4a21ba3d0a43c3e7a046fda9b9
Moises Miguel <moimigueldev@gmail.com>
2019-11-17 20:24:18 -0600

    matching and substituting

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const port = 3000;
 
 //modules
 const firstReverse = require('./modules/first-reverse');
-const whosNext = require('./modules/whos-next');
+const change = require('./modules/matching-subs');
 
-console.log(whosNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 52))
-// console.log(whosNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 7230702951))
-// console.log(whosNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 52))
+const s1 = 'Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-0091\nDate: Tues April 9, 2005\nVersion: 6.7\nLevel: Alpha'
+const s11 = 'Program title: Hammer\nAuthor: Tolkien\nCorporation: IB\nPhone: +1-503-555-0090\nDate: Tues March 29, 2017\nVersion: 2.0\nLevel: Release'
+const s12 = 'Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-009\nDate: Tues April 9, 2005\nVersion: 6.7\nLevel: Alpha'
+
+
+console.log(change(s1, 'Ladder', '1.1'))
+console.log(change(s11, 'Balance', '1.5.6'))
+// console.log(change(s12, 'Ladder', '1.1'))
+
 
 app.get('/', async (req, res) => {
     console.log('Main Url hit');
