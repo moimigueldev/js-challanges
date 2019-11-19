aa4a578fb6124094c9b9e952105da8686d5d0846
Moises Miguel <moimigueldev@gmail.com>
2019-11-18 19:57:10 -0600

    sum of pairs

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-//modules
-const firstReverse = require('./modules/first-reverse');
-const divisibleCounts = require('./modules/count-divisibles');
 
-// console.log(divisibleCounts(6, 11, 2))
-console.log(divisibleCounts(101, 9223372036854775806, 11))
 
 
 
