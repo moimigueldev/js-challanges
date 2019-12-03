f42f4307931baad24f35bcd5ac498f8b3a06db13
Moises Miguel <moimigueldev@gmail.com>
2019-12-02 18:31:12 -0600

    cleaned up index

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const port = 4000;
 
 const test = require('./modules/november/deaf-rats');
 
-// console.log(test('~O~O~O~O P'))
-console.log(test('~O~O~O~OO~P~O~OO~'))
-// console.log(test('P O~ O~ ~O O~'))
 
-// console.log(test('~O~O~O  ~O~O  O~~O~O~O~O  ~O~O~O~OO~~O~O~O~O~O~O  ~O~O~O~OO~  O~P'))
 
 
 app.get('/', async (req, res) => {
