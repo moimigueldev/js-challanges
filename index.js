3686aad4b73928fe557746c18d8023ffdc6d0495
Moises Miguel <moimigueldev@gmail.com>
2019-12-23 21:10:42 -0500

    shortest word

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-const test = require('./modules/december/sum-nums-digits');
+const test = require('./modules/december/shortest-word');
 
-console.log(test(-32));
+console.log(test('bitcoin take over the world maybe who knows perhaps'));
 
 app.get('/', async (req, res) => {
   console.log('Main Url hit');
---
+++ modules/december/shortest-word.js
@@ -0,0 +1,13 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+
+// Test.describe("Example tests",_=>{
+//   Test.assertEquals(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
+//   Test.assertEquals(findShort("turns out random test cases are easier than writing out basic ones"), 3);
+//   });
+
+function findShort(s) {
+  return s.split(' ').reduce((a, b) => (b.length >= a.length ? a : b)).length;
+}
+
+module.exports = findShort;
