7ac06c202ab02b8f4a7e7e82598d6509f22141f9
moises <moimigueldev@gmail.com>
2019-12-26 19:33:59 -0500

    cleaned up index.html\

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const port = 4000;
 
 const test = require('./modules/december/find-the-parity');
 
-// console.log(test([160, 3, 1719, 19, 11, 13, -21]));
-console.log(test([1, 1, 0, 1, 1]));
-
 app.get('/', async (req, res) => {
   console.log('Main Url hit');
   res.send('JS-Challange modules');
