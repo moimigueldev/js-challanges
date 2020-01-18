db0e574ef12eb29b2179d650cad22e020ab5905a
moises <moimigueldev@gmail.com>
2020-01-18 12:32:24 -0600

    created digital root project

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-const test = require('./modules/january/array-diff');
+const test = require('./modules/january/digital-roots');
+
+console.log(test(942))
 
 
 
