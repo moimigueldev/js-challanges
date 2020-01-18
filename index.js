0455c104bdb37ebc4e8ff3177066355f0ec307fa
moises <moimigueldev@gmail.com>
2020-01-17 18:41:18 -0600

    created the january folder

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-const test = require('./modules/december/find-the-parity');
+const test = require('./modules/january/array-diff');
+
+console.log(test([1,2,2,2,3],[2]))
 
 app.get('/', async (req, res) => {
   console.log('Main Url hit');
