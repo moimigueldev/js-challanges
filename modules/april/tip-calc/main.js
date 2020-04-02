ced250222d3e62ac63e3bf688a8391bd17868a5f
moises <moimigueldev@gmail.com>
2020-04-02 12:39:44 -0600

    gettin the value for the tip

---
+++ modules/april/tip-calc/main.js
@@ -0,0 +1,10 @@
+let tipRange = document.getElementById('tipAmount');
+let mealCost = document.getElementById('mealCost');
+
+const totalCost = 0
+document.getElementById('button').addEventListener('click', function () {
+
+    console.log('total', mealCost.value * Number(`.${tipRange.value}`))
+    console.log(Number(`.${tipRange.value}`))
+
+});
