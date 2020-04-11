5f406d875137c1f34ce0daf5b785ebd27a97c979
moises <moimigueldev@gmail.com>
2020-04-11 14:10:25 -0600

    cleaned up the console logs

--- modules/april/tip-calc/main.js
+++ modules/april/tip-calc/main.js
@@ -4,7 +4,7 @@ let mealCost = document.getElementById('mealCost');
 const totalCost = 0
 document.getElementById('button').addEventListener('click', function () {
 
-    console.log('total', mealCost.value * Number(`.${tipRange.value}`))
+    // console.log('total', mealCost.value * Number(`.${tipRang.value}`))
     // console.log(Number(`.${tipRange.value}`))
 
 });
