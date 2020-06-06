4b319f1a9ee76ca18548918916922c19dec770a6
moises <moimigueldev@gmail.com>
2020-06-05 22:01:52 -0500

    reviewed

--- modules/april/tip-calc/main.js
+++ modules/april/tip-calc/main.js
@@ -2,9 +2,9 @@ let tipRange = document.getElementById('tipAmount');
 let mealCost = document.getElementById('mealCost');
 
 const totalCost = 0
-// document.getElementById('button').addEventListener('click', function () {
+document.getElementById('button').addEventListener('click', function () {
 
 
 
 
-// }); 
+}); 
