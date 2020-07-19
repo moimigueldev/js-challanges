9e0f2da723c6caf32df4dea4baf465cc8f00ed9d
moises <moimigueldev@gmail.com>
2020-07-19 17:23:42 -0500

    'solved'

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-const test = require('./modules/june/sum')
+const test = require('./modules/july/casino-chips')
 
-console.log(test(25))
+// console.log(test([4, 1, 1]))
+console.log(test([1, 2, 1]))
+// console.log(test([1, 1, 1]))
 
 
 app.get('/', async (req, res) => {
