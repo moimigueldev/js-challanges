d8233e3689df440c0825c8798a1dd9cec1e73cfa
moises <moimigueldev@gmail.com>
2020-07-20 22:25:48 -0500

    solved and refactored

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-const test = require('./modules/july/short-long')
+const test = require('./modules/july/fix-string-case')
 
-console.log('test', test('22', '1'))
+// console.log('test', test('Code'))
+console.log('test', test('coDE'))
 
 
 app.get('/', async (req, res) => {
