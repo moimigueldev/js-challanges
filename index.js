54a8660bd35fc2b3a1510ba49485af464a9a2093
moises <moimigueldev@gmail.com>
2020-07-30 19:22:24 -0500

    cleaned up index file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
-const test = require("./modules/july/longest-vowel-chain");
+// const test = require("./modules/july/longest-vowel-chain");
 
 
 
