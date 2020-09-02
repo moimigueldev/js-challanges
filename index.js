73a8fd4a4f56ec6ea60c2cb8c470e2f10510d469
moises <moimigueldev@gmail.com>
2020-09-01 21:02:38 -0500

    code challange

--- index.js
+++ index.js
@@ -3,6 +3,37 @@ const app = express();
 const port  = 4000;
 
 
+function dna(s) {
+
+  let newString = '';
+
+  for (let index = 0; index < s.length; index++) {
+    if(s[index] === 'a') {
+        newString = newString + 't'
+    }
+
+    if (s[index] === 't') {
+      newString = newString +'a'
+    }
+
+    if (s[index] === 'c') {
+      newString = newString +'g'
+    } 
+
+    if (s[index] === 'g') {
+      newString = newString +'c'
+    }
+    
+  }
+
+  return newString
+
+}
+
+console.log(dna('ataattccgg'))
+
+
+
 app.get("/", async (req, res) => {
   res.send("JS-Challange modules");
 });
