ebf104e67abb2fd492446d035c4d894269395b2f
moises <moimigueldev@gmail.com>
2020-09-22 11:12:15 -0500

    cleared index

--- index.js
+++ index.js
@@ -3,35 +3,6 @@ const app = express();
 const port  = 4000;
 
 
-function dna(s) {
-
-  let newString = '';
-
-  for (let index = 0; index < s.length; index++) {
-  
-    if(s[index] === 'a') {
-        newString = newString + 't'
-    }
-
-    if (s[index] === 't') {
-      newString = newString +'a'
-    }
-
-    if (s[index] === 'c') {
-      newString = newString +'g'
-    } 
-
-    if (s[index] === 'g') {
-      newString = newString +'c'
-    }
-    
-  }
-
-  return newString
-
-}
-
-console.log(dna('ataattccgg'))
 
 
 
