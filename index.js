bfa7509a2d79696765aa036122ca28539341eb50
moises <moimigueldev@gmail.com>
2020-10-08 11:44:26 -0500

    cleaned up index

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port  = 4000;
-
-
-
-
-
-app.get("/", async (req, res) => {
-  res.send("JS-Challange modules");
+const port = 4000;
+app.get('/', async (req, res) => {
+  res.send('JS-Challange modules');
 });
 
-app.listen(port, () => console.log("app on port", port));
+app.listen(port, () => console.log('app on port', port));
